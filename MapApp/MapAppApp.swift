c047ee34fc29b3177653b272e200feb5f53ee326
Fatih <fatihk.96brs@gmail.com>
2024-06-14 17:21:12 +0300

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Fatih Kenarda on 14.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
